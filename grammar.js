92a98adaa534957b9a70b03e9acb9ccf9345033a
gbprod <contact@gb-prod.fr>
2023-10-03 03:40:05 +0200

    fix: multiple modifiers on a class

--- grammar.js
+++ grammar.js
@@ -284,7 +284,8 @@ module.exports = grammar({
 
     class_declaration: $ => prec.right(seq(
       optional(field('attributes', $.attribute_list)),
-      optional(field('modifier', choice($.final_modifier, $.abstract_modifier, $.readonly_modifier))),
+      optional(field('modifier', choice($.final_modifier, $.abstract_modifier))),
+      optional(field('modifier', $.readonly_modifier)),
       keyword('class'),
       field('name', $.name),
       optional($.base_clause),
