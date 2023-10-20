657546dd3e99729df510cc7f17c5871e918607c0
Caleb White <cdwhite3@pm.me>
2023-10-20 09:08:49 +0200

    Split parser into php/php_only dialects with common files

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -1,19 +1,29 @@
 fn main() {
-    let src_dir = std::path::Path::new("src");
+    let root_dir = std::path::Path::new(".");
+    let php_dir = root_dir.join("php").join("src");
+    let php_only_dir = root_dir.join("php_only").join("src");
 
     let mut c_config = cc::Build::new();
-    c_config.include(src_dir);
+    c_config.include(&php_dir);
     c_config
         .flag_if_supported("-Wno-unused-parameter")
         .flag_if_supported("-Wno-unused-but-set-variable")
         .flag_if_supported("-Wno-trigraphs");
-    let parser_path = src_dir.join("parser.c");
-    c_config.file(&parser_path);
 
-    let scanner_path = src_dir.join("scanner.c");
-    c_config.file(&scanner_path);
-    println!("cargo:rerun-if-changed={}", scanner_path.to_str().unwrap());
+    for path in &[
+        php_dir.join("parser.c"),
+        php_dir.join("scanner.c"),
+        php_only_dir.join("parser.c"),
+        php_only_dir.join("scanner.c"),
+    ] {
+        c_config.file(&path);
+        println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+    }
 
-    println!("cargo:rerun-if-changed={}", parser_path.to_str().unwrap());
-    c_config.compile("parser");
+    println!(
+        "cargo:rerun-if-changed={}",
+        root_dir.join("common").join("scanner.h").to_str().unwrap()
+    );
+
+    c_config.compile("parser-scanner");
 }
