2f791d9d83c35b11e9a14daf9a58dd7e23566041
Caleb White <cdwhite3@pm.me>
2024-01-05 11:48:10 -0600

    feat: add injection queries for heredocs and comments

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,3 +1,14 @@
 ((text) @injection.content
  (#set! injection.language "html")
  (#set! injection.combined))
+
+((comment) @injection.content
+  (#set! injection.language "phpdoc"))
+
+(heredoc
+  (heredoc_body) @injection.content
+  (heredoc_end) @injection.language)
+
+(nowdoc
+  (nowdoc_body) @injection.content
+  (heredoc_end) @injection.language)
