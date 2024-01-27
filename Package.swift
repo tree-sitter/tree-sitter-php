b75c767f474c992d6cf02df6b184a425eb63565b
Caleb White <cdwhite3@pm.me>
2024-01-26 23:36:42 -0600

    0.21.0

--- Package.swift
+++ Package.swift
@@ -2,26 +2,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "TreeSitterPHP",
-    products: [
-        .library(name: "TreeSitterPHP", targets: ["TreeSitterPHP"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "TreeSitterPHP",
-                path: ".",
-                exclude: [
-                ],
-                sources: [
-                    "php/src/parser.c",
-                    "php/src/scanner.c",
-                    "php_only/src/parser.c",
-                    "php_only/src/scanner.c",
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("php/src")])
-    ]
+  name: "TreeSitterPHP",
+  products: [
+    .library(name: "TreeSitterPHP", targets: ["TreeSitterPHP"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "TreeSitterPHP",
+      path: ".",
+      exclude: [
+      ],
+      sources: [
+        "php/src/parser.c",
+        "php/src/scanner.c",
+        "php_only/src/parser.c",
+        "php_only/src/scanner.c",
+      ],
+      resources: [
+        .copy("queries")
+      ],
+      publicHeadersPath: "bindings/swift",
+      cSettings: [.headerSearchPath("php/src")]
+    )
+  ]
 )
