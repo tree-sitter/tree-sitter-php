ba42dc9e819c12dbc15108aa645d14fb9d82cd7f
Caleb White <cdwhite3@pm.me>
2024-02-21 15:26:47 -0600

    chore: bump cli version

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,24 @@
 (php_tag) @tag
 "?>" @tag
 
+
+; Variables
+
+(relative_scope) @variable.builtin
+
+(variable_name) @variable
+
+((name) @constant
+ (#match? @constant "^_?[A-Z][A-Z\\d_]+$"))
+((name) @constant.builtin
+ (#match? @constant.builtin "^__[A-Z][A-Z\d_]+__$"))
+
+((name) @constructor
+ (#match? @constructor "^[A-Z]"))
+
+((name) @variable.builtin
+ (#eq? @variable.builtin "this"))
+
 ; Types
 
 (primitive_type) @type.builtin
@@ -37,23 +55,6 @@
   name: (variable_name (name)) @property)
 (member_access_expression
   name: (name) @property)
-
-; Variables
-
-(relative_scope) @variable.builtin
-
-((name) @constant
- (#match? @constant "^_?[A-Z][A-Z\\d_]+$"))
-((name) @constant.builtin
- (#match? @constant.builtin "^__[A-Z][A-Z\d_]+__$"))
-
-((name) @constructor
- (#match? @constructor "^[A-Z]"))
-
-((name) @variable.builtin
- (#eq? @variable.builtin "this"))
-
-(variable_name) @variable
 
 ; Basic tokens
 [
